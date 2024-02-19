7140f08038624f7a000ae2c1dc61130eb0106bf4
phamhiep2506 <phamhiep25062k@gmail.com>
2024-02-20 00:58:13 +0700

    remove Controller.java & add Input.java

--- KM2Game.java
+++ KM2Game.java
@@ -1,10 +1,15 @@
 package KM2Game;
 
+import android.view.MotionEvent;
+
 public class KM2Game {
 
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
+
+        Input.injectTouch(MotionEvent.ACTION_DOWN, 0, 10, 540, 1170);
+        Input.injectTouch(MotionEvent.ACTION_UP, 0, 0, 540, 1170);
 
     }
 
