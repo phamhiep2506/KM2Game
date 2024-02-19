0c5b79e9aeed3ccb0a79c4c39fc68d22e7270470
phamhiep2506 <phamhiep25062k@gmail.com>
2024-02-19 16:01:26 +0700

    update KM2Game.c

--- Android.mk
+++ Android.mk
@@ -2,15 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := \
-	libevdev/libevdev/libevdev.c \
-	libevdev/libevdev/libevdev-uinput.c \
-	libevdev/libevdev/libevdev-names.c \
-	KM2Game.c
-
-LOCAL_C_INCLUDES := \
-    $(LOCAL_PATH)/libevdev/include
-
+LOCAL_SRC_FILES := KM2Game.c
 LOCAL_MODULE := KM2Game
 LOCAL_CFLAGS := -Wall
 
--- Makefile
+++ Makefile
@@ -8,6 +8,3 @@ build:
 
 clean:
 	rm -rf libs obj
-
-debug:
-	adb push libs/arm64-v8a/KM2Game /data/local/tmp
