5bbaa109ed0f9cd1666b0325e1596b111b9ea8f3
phamhiep2506 <phamhiep25062k@gmail.com>
2024-02-20 23:19:05 +0700

    add call inputEvent

--- server/KM2Game.java
+++ server/KM2Game.java
@@ -4,25 +4,34 @@ import android.view.MotionEvent;
 
 public class KM2Game {
 
+    public static final int UP = 0, DOWN = 1, MOVE = 2;
+
     static {
         System.load("/data/local/tmp/libKM2Game.so");
     }
 
-    private native void sayHello();
+    private native void startConvert();
+
+    public void injectEvent(float x, float y, int action, int pointerId) {
+        Input input = new Input();
+        switch(action) {
+            case UP:
+                input.injectTouch(MotionEvent.ACTION_UP, pointerId, 0.0f, x, y);
+                break;
+            case DOWN:
+                input.injectTouch(MotionEvent.ACTION_DOWN, pointerId, 1.0f, x, y);
+                break;
+            case MOVE:
+                input.injectTouch(MotionEvent.ACTION_MOVE, pointerId, 1.0f, x, y);
+                break;
+        }
+    }
 
     public static void main(String[] args) throws InterruptedException {
 
         KM2Game km2game = new KM2Game();
 
-        km2game.sayHello();
-
-    // Input input = new Input();
-
-    // input.injectTouch(MotionEvent.ACTION_DOWN, 1, 1.0f, 250.0f, 500.0f);
-    // input.injectTouch(MotionEvent.ACTION_DOWN, 2, 1.0f, 800.0f, 500.0f);
-
-    // input.injectTouch(MotionEvent.ACTION_UP, 1, 1.0f, 250.0f, 500.0f);
-    // input.injectTouch(MotionEvent.ACTION_UP, 2, 1.0f, 800.0f, 500.0f);
+        km2game.startConvert();
 
     }
 
--- server/jni/KM2Game.c
+++ server/jni/KM2Game.c
@@ -1,8 +1,14 @@
 #include <jni.h>
-#include <stdio.h>
 #include <android/log.h>
 
 JNIEXPORT void JNICALL
-Java_KM2Game_KM2Game_sayHello(JNIEnv *env, jobject obj) {
-   __android_log_print(ANDROID_LOG_INFO, "KM2Game", "sayHello()");
+Java_KM2Game_KM2Game_startConvert(JNIEnv *env, jobject obj) {
+
+   __android_log_print(ANDROID_LOG_INFO, "KM2Game", "Start Convert");
+
+   jclass cls = (*env)->GetObjectClass(env, obj);
+   jmethodID injectEvent = (*env)->GetMethodID(env, cls, "injectEvent", "(FFII)V");
+   (*env)->CallVoidMethod(env, obj, injectEvent, (float)540, (float)1170, 1, 1);
+   (*env)->CallVoidMethod(env, obj, injectEvent, (float)540, (float)1170, 0, 1);
+
 }
