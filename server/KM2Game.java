0a95f392a644e258779adde4c92b3d5fbf632232
phamhiep2506 <phamhiep25062k@gmail.com>
2024-02-21 09:51:07 +0700

    update KM2Game.java

--- server/KM2Game.java
+++ server/KM2Game.java
@@ -4,16 +4,16 @@ import android.view.MotionEvent;
 
 public class KM2Game {
 
-    public static final int UP = 0, DOWN = 1, MOVE = 2;
-
     static {
         System.load("/data/local/tmp/libKM2Game.so");
     }
 
     private native void startConvert();
 
+    public static final int UP = 0, DOWN = 1, MOVE = 2;
+    public static final Input input = new Input();
+
     public void injectEvent(float x, float y, int action, int pointerId) {
-        Input input = new Input();
         switch(action) {
             case UP:
                 input.injectTouch(MotionEvent.ACTION_UP, pointerId, 0.0f, x, y);
