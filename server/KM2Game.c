cbf3da37a3aed74572b4225631976cd80b3a1ac5
phamhiep2506 <phamhiep25062k@gmail.com>
2024-02-26 13:19:09 +0700

    add code c mouse aim

--- server/KM2Game.c
+++ server/KM2Game.c
@@ -8,10 +8,16 @@
 
 #define TOUCH_DEVPATH "/dev/input/event2"
 #define MOUSE_DEVPATH "/dev/input/event5"
+#define CENTER_X 540
+#define CENTER_Y 1170
 
 int touch_fd, mouse_fd;
-int last_x = 540;
-int last_y = 1170;
+int last_x = CENTER_X;
+int last_y = CENTER_Y;
+int min_x = 0;
+int min_y = 0;
+int max_x = 1080;
+int max_y = 2340;
 
 void write_event(int fd, int type, int code, int value) {
    struct input_event ev;
@@ -38,17 +44,52 @@ int open_devpath(char *path)
     return fd;
 }
 
-void handle_mouse_event(int fd, struct input_event *ev) {
+void mouse_aim_start(int fd) {
+   write_event(fd, EV_ABS, ABS_MT_SLOT, 0);
+   write_event(fd, EV_ABS, ABS_MT_TRACKING_ID, 100);
+   write_event(fd, EV_KEY, BTN_TOUCH, 1);
+   write_event(fd, EV_ABS, ABS_MT_POSITION_X, CENTER_X);
+   write_event(fd, EV_ABS, ABS_MT_POSITION_Y, CENTER_Y);
+   write_event(fd, EV_ABS, ABS_MT_TOUCH_MAJOR, 10);
+   write_event(fd, EV_SYN, SYN_REPORT, 0);
+}
+
+void mouse_aim_reset(int fd) {
+   write_event(fd, EV_ABS, ABS_MT_SLOT, 0);
+   write_event(fd, EV_ABS, ABS_MT_TOUCH_MAJOR, 0);
+   write_event(fd, EV_ABS, ABS_MT_TRACKING_ID, -1);
+   write_event(fd, EV_KEY, BTN_TOUCH, 0);
+   write_event(fd, EV_SYN, SYN_REPORT, 0);
+
+}
+
+void convert_mouse_event(int fd, struct input_event *ev) {
     switch(ev->type) {
         case EV_REL:
             switch(ev->code) {
                 case REL_X:
-                    last_x = last_x + ev->value;
-                    write_event(fd, EV_ABS, ABS_MT_POSITION_X, last_x);
+                    last_y = last_y - ev->value;
+                    if(last_y > max_y || last_y < min_y) {
+                        mouse_aim_reset(fd);
+                        mouse_aim_start(fd);
+                        last_x = CENTER_X;
+                        last_y = CENTER_Y;
+                        write_event(fd, EV_ABS, ABS_MT_POSITION_Y, last_y);
+                    } else {
+                        write_event(fd, EV_ABS, ABS_MT_POSITION_Y, last_y);
+                    }
                     break;
                 case REL_Y:
-                    last_y = last_y + ev->value;
-                    write_event(fd, EV_ABS, ABS_MT_POSITION_Y, last_y);
+                    last_x = last_x + ev->value;
+                    if(last_x > max_x || last_x < min_x) {
+                        mouse_aim_reset(fd);
+                        mouse_aim_start(fd);
+                        last_x = CENTER_X;
+                        last_y = CENTER_Y;
+                        write_event(fd, EV_ABS, ABS_MT_POSITION_X, last_x);
+                    } else {
+                        write_event(fd, EV_ABS, ABS_MT_POSITION_X, last_x);
+                    }
                     break;
             }
             break;
@@ -66,17 +107,11 @@ int main() {
    touch_fd = open_devpath(TOUCH_DEVPATH);
    mouse_fd = open_devpath(MOUSE_DEVPATH);
 
-   write_event(touch_fd, EV_ABS, ABS_MT_SLOT, 0);
-   write_event(touch_fd, EV_ABS, ABS_MT_TRACKING_ID, 100);
-   write_event(touch_fd, EV_KEY, BTN_TOUCH, 1);
-   write_event(touch_fd, EV_ABS, ABS_MT_POSITION_X, last_x);
-   write_event(touch_fd, EV_ABS, ABS_MT_POSITION_Y, last_y);
-   write_event(touch_fd, EV_ABS, ABS_MT_TOUCH_MAJOR, 10);
-   write_event(touch_fd, EV_SYN, SYN_REPORT, 0);
+   mouse_aim_start(touch_fd);
 
    while(1) {
         if(read(mouse_fd, &ev, sizeof(ev)) == sizeof(ev)) {
-            handle_mouse_event(touch_fd, &ev);
+            convert_mouse_event(touch_fd, &ev);
         }
    }
 
