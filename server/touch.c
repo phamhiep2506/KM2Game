11c7b8a56bb4bf90a8145261226924d0426453e3
phamhiep2506 <phamhiep25062k@gmail.com>
2024-03-08 01:51:24 +0700

    add support multitouch screen

--- server/touch.c
+++ server/touch.c
@@ -2,15 +2,44 @@
 #include "config.h"
 #include <linux/input-event-codes.h>
 
+#include <stdlib.h>
+#include <stdbool.h>
+
 struct mt_touch {
     int mt_tracking_id;
     int mt_slot_total;
+    int slot[MAX_ABS_MT_SLOT];
 };
 
 void mt_touch_down(int touch_fd, struct mt_touch *mt_touch, int *slot, int x, int y) {
     /* slot */
     mt_touch->mt_slot_total = mt_touch->mt_slot_total + 1;
-    *slot = mt_touch->mt_slot_total;
+
+    /* ramdom slot */
+    int ramdom_slot;
+    bool check_duplicate;
+    int arr_length = sizeof(mt_touch->slot) / sizeof(int);
+    do {
+        check_duplicate = false;
+        /* int r = minN + rand() % (maxN + 1 - minN) */
+        ramdom_slot = 1 + rand() % (9 + 1 - 1);
+        for(int i = 0; i < arr_length; i++) {
+            if(mt_touch->slot[i] == ramdom_slot) {
+                check_duplicate = true;
+                break;
+            }
+        }
+    } while(check_duplicate);
+
+    /* add ramdom slot */
+    for(int i = 0; i < arr_length; i++) {
+        if(mt_touch->slot[i] == 0) {
+            mt_touch->slot[i] = ramdom_slot;
+            break;
+        }
+    }
+
+    *slot = ramdom_slot;
     write_event(touch_fd, EV_ABS, ABS_MT_SLOT, *slot);
 
     /* tracking id */
@@ -33,6 +62,7 @@ void mt_touch_down(int touch_fd, struct mt_touch *mt_touch, int *slot, int x, in
 }
 
 void mt_touch_up(int touch_fd, struct mt_touch *mt_touch, int *slot) {
+
     write_event(touch_fd, EV_ABS, ABS_MT_SLOT, *slot);
     write_event(touch_fd, EV_ABS, ABS_MT_TOUCH_MAJOR, 0);
     write_event(touch_fd, EV_ABS, ABS_MT_TRACKING_ID, -1);
@@ -43,7 +73,18 @@ void mt_touch_up(int touch_fd, struct mt_touch *mt_touch, int *slot) {
     }
 
     write_event(touch_fd, EV_SYN, SYN_REPORT, 0);
+
     mt_touch->mt_slot_total = mt_touch->mt_slot_total - 1;
+
+    /* remove slot */
+    int arr_length = sizeof(mt_touch->slot) / sizeof(int);
+    for(int i = 0; i < arr_length; i++) {
+        if(mt_touch->slot[i] == *slot) {
+            mt_touch->slot[i] = 0;
+            break;
+        }
+    }
+
     *slot = 0;
 }
 
--- server/touch.h
+++ server/touch.h
@@ -1,9 +1,12 @@
 #ifndef TOUCH_H
 #define TOUCH_H
 
+#include "config.h"
+
 struct mt_touch {
     int mt_tracking_id;
     int mt_slot_total;
+    int slot[MAX_ABS_MT_SLOT];
 };
 
 void mt_touch_down(int touch_fd, struct mt_touch *mt_touch, int *slot, int x, int y);
